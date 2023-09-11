61d4ede7939ab123b31cf5a391baeb7919c6ce3e
secondary <l.bitti98@gmail.com>
2023-09-11 13:31:31 +0100

    created a test
    the file also contains descriptions and explanations of the test

--- Unit_Testing_Course/src/test/java/com/javaeasily/demos/junit/CalculatorTest.java
+++ Unit_Testing_Course/src/test/java/com/javaeasily/demos/junit/CalculatorTest.java
@@ -1,5 +1,43 @@
 package com.javaeasily.demos.junit;
+import org.junit.Assert;
+import org.junit.Test;
 
+import javax.xml.bind.ValidationException;
+
+//example test
 public class CalculatorTest {
 
+    @Test
+    public void testAdd(){
+
+        Assert.assertNotNull(new Object());
+        Assert.assertTrue(10 == 10);
+        Assert.assertNotEquals(1L,2L);
+    }
+
+
+    /*
+    a nut test should be divided into 3 sections
+
+    arrange - we start a setup, creating objects that the test might need/data/mocks/mocks objects
+
+    act - one line of code, method that's bring tested
+
+    assert - this is few assertions check the state of the world after they have been executed
+
+     */
+
+
+
+
+//    @Test
+//    public void testAdd(){
+//        try{
+//            //Give bad input to SUT
+//            Assert.fail("failing the test");
+//        }
+//        catch (ValidationException e){
+//
+//        }
+//    }
 }
