b5723b0d0df4fbbce745c80791c4c8f95f56cef2
Ludovico98 <l.bitti98@gmail.com>
2023-09-21 16:16:04 +0100

    Update UserLookupServicesTest.java
    it's working fine, can check precisely if the users username and password match the once which are in the list.

--- Unit_Testing_Course/src/test/java/com/javaeasily/demos/mockito/myapp/service/UserLookupServicesTest.java
+++ Unit_Testing_Course/src/test/java/com/javaeasily/demos/mockito/myapp/service/UserLookupServicesTest.java
@@ -2,6 +2,7 @@ package com.javaeasily.demos.mockito.myapp.service;
 
 import com.javaeasily.demos.mockito.myapp.User;
 import com.javaeasily.demos.mockito.myapp.data.UserRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,7 +13,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserLookupServicesTest {
@@ -24,13 +26,11 @@ public class UserLookupServicesTest {
     @Test
     public void getRegularUsers() {
 
-        System.out.println(User.str);
-
         //arrange
         List<User> userList = new LinkedList<>();
         System.out.println("I have added something!!!");
 
-        userList.add(User.createRegularUser("marco" , "marco"));
+        userList.add(User.createRegularUser("marco" , "marco123"));
         userList.add(User.createAdminUser("gianni" , "marco"));
 
         when(userRepository.findAll()).thenReturn(userList);
@@ -38,7 +38,30 @@ public class UserLookupServicesTest {
 
         Set<User> actualUsers = userLookupServices.getRegularUsers();
 
-        //assert
+        //assert - general collections
+        assertNotNull(actualUsers);
+        assertEquals(actualUsers.size(),1);
+
+//        USED TO CHECK THE NUMBER OF USERS IN THE LIST
+//        for (User actualUser:actualUsers){
+//            assertNotNull(actualUser);
+//            assertEquals(actualUser.getUserType(), User.UserType.REGULAR_USER);
+//        }
+
+        //check user 1
+        User actualUser1 = userList.get(0);
+        assertNotNull(actualUser1);
+        assertEquals(actualUser1.getUserType(), User.UserType.REGULAR_USER);
+        assertEquals(actualUser1.getUsername(), "marco");
+        assertEquals(actualUser1.getPassword(), "marco123");
+
+        //check user 2
+        User actualUser2 = userList.get(1);
+        assertNotNull(actualUser1);
+        assertEquals(actualUser2.getUserType(), User.UserType.ADMIN_USER);
+        assertEquals(actualUser2.getUsername(), "gianni");
+        assertEquals(actualUser2.getPassword(), "marco");
+
     }
     @Test
     public void getAdminUsers() {
