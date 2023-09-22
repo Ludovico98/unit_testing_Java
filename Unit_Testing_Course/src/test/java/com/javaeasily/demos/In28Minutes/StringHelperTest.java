bbf5728a4bee5edc2c11711ae992cb4c5b5f066f
Ludovico98 <l.bitti98@gmail.com>
2023-09-22 10:11:50 +0100

    New course
    Following a new course, instead of creating a different repository. I have created a new package.

---
+++ Unit_Testing_Course/src/test/java/com/javaeasily/demos/In28Minutes/StringHelperTest.java
@@ -0,0 +1,16 @@
+package com.javaeasily.demos.In28Minutes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringHelperTest extends Object {
+
+    @Test
+    public void truncateAInFirst2Positions() {
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame() {
+    }
+}
