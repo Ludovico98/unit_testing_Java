48f8f3418029ab813c2a5083618a72a2dccad1f9
Ludovico98 <l.bitti98@gmail.com>
2023-09-22 14:12:57 +0100

    This runs all the tests in one go

---
+++ Unit_Testing_Course/src/test/java/com/javaeasily/demos/In28Minutes/AllTests.java
@@ -0,0 +1,9 @@
+package com.javaeasily.demos.In28Minutes;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({QuickBeforeAfterTest.class,StringHelperTest.class})
+public class AllTests {
+
+}
