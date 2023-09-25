6656b963bef8b0a3209fb20954b3fdc10df3a50f
Ludovico98 <l.bitti98@gmail.com>
2023-09-25 14:27:53 +0100

    Update TodoBusinessImplTest.java
    update

--- Unit_Testing_Course/src/test/java/com/javaeasily/demos/In28Minutes/TodoBusinessImplTest.java
+++ Unit_Testing_Course/src/test/java/com/javaeasily/demos/In28Minutes/TodoBusinessImplTest.java
@@ -2,6 +2,7 @@ package com.javaeasily.demos.In28Minutes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -19,24 +20,8 @@ public class TodoBusinessImplTest {
 
     @Mock
     TodoService todoServiceMock;
-
-    @Test
-    public void testRetrieveTodosRelatedToSpring_usingBDD() {
-
-        //Given
-        TodoService todoServiceMock = mock(TodoService.class);
-        List<String> todos = Arrays.asList("learn spring `MVC", "learning spring", "learn to dance");
-
-        given(todoServiceMock.retriveTodos("Dummy")).willReturn(todos);
-        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-
-        //When
-        List<String> filteredTodos = todoBusinessImpl.retriveTodosRelatedToSpring("Dummy");
-
-        //Then
-        assertThat(filteredTodos.size(), is(2) );
-
-    }
+    @InjectMocks
+    TodoBusinessImpl todoBusinessImpl;
 
     @Test
     public void testDeleteTodosNotRelatedToSpring_usingBDD() {
@@ -45,7 +30,6 @@ public class TodoBusinessImplTest {
         List<String> todos = Arrays.asList("learn spring MVC", "learning spring", "learn to dance");
 
         given(todoServiceMock.retriveTodos("Dummy")).willReturn(todos);
-        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
 
         //When
         todoBusinessImpl.deleteTodosNotRelatedToSpring("Dummy");
@@ -63,5 +47,23 @@ public class TodoBusinessImplTest {
         verify(todoServiceMock, never()).deleteTodo("learn spring");
         then(todoServiceMock).should(never()).deleteTodo("learn spring");
     }
+
+//    @Test
+//    public void testRetrieveTodosRelatedToSpring_usingBDD() {
+//
+//        //Given
+//        TodoService todoServiceMock = mock(TodoService.class);
+//        List<String> todos = Arrays.asList("learn spring `MVC", "learning spring", "learn to dance");
+//
+//        given(todoServiceMock.retriveTodos("Dummy")).willReturn(todos);
+//        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+//
+//        //When
+//        List<String> filteredTodos = todoBusinessImpl.retriveTodosRelatedToSpring("Dummy");
+//
+//        //Then
+//        assertThat(filteredTodos.size(), is(2) );
+//
+//    }
 
 }
